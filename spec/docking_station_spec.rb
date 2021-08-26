45649e28d35faa7c9863eec935eab1f16c2554f6
Amanda Vuong <mandyvuong@gmail.com>
2021-08-26 20:13:47 +0100

    step 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,18 @@ require 'docking_station'
 describe DockingStation do 
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do 
-    bike = subject.release_bike
-    expect(bike).to be_working # Predicate matchers - passes if bike.working?
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
+
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
   # dock bike at station
