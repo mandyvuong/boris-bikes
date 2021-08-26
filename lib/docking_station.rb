30fb642589d4dad5c925a6e4f2d2e31c46b50107
Amanda Vuong <mandyvuong@gmail.com>
2021-08-26 20:46:39 +0100

    step 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,12 +6,23 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if full?
     @bikes << bike
+  end
+
+  # https://www.culttt.com/2015/06/03/the-difference-between-public-protected-and-private-methods-in-ruby/
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
