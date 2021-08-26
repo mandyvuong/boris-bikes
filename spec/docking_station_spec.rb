340e48259a2cdda167f5a64682495415c343d0e0
Amanda Vuong <mandyvuong@gmail.com>
2021-08-26 19:49:27 +0100

    step 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,4 +8,21 @@ describe DockingStation do
     expect(bike).to be_working # Predicate matchers - passes if bike.working?
   end
 
+  # dock bike at station
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  # bike that's been docked
+  it { is_expected.to respond_to :bike }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike # return bike to dock
+  end
+
 end
