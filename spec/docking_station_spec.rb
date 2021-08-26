86f2a5318be2d4e7b9a7569184e05f5cb66c9f74
Amanda Vuong <mandyvuong@gmail.com>
2021-08-26 20:50:00 +0100

    step 16

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -21,7 +21,9 @@ describe DockingStation do
 
   describe '#dock' do # limits to dock bike once
     it 'raises an error when full' do
-      20.times { subject.dock Bike.new }
+      DockingStation::DEFAULT_CAPACITY.times do
+        subject.dock Bike.new
+      end
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
